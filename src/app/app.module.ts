8b377a68ee0d1f81006de03f7995ce29ba87a7c0
Àngel David <dpuerto666@gmail.com>
2021-03-14 14:10:51 -0500

    momdulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contadores/contador.component';
-import {  TituloComponent } from './frase-titulo/titulo.component';
-import {  FraseComponent } from './frase-titulo/frase.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
-
+import { TituloComponent } from './frase-titulo/titulo.component';
+import { FraseComponent } from './frase-titulo/frase.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContadorComponent,
-    TituloComponent,
-    FraseComponent,
-    HeroeComponent,
-    ListadoComponent,
-  
-  ],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FraseComponent, TituloComponent, ContadorComponent],
+  imports: [BrowserModule, HeroesModule], // ContadoresModule, FraseTituloModule
   providers: [],
   bootstrap: [AppComponent],
 })
